895016f08d8bb0bd57983d229fb4eabc82878a95
Arya Permadi <aryaper7766@gmail.com>
2026-01-14 16:23:27 +0700

    Update koneksi.php

--- includes/koneksi.php
+++ includes/koneksi.php
@@ -1,15 +1,10 @@
 <?php
-// =============================
-// Koneksi Database (PROSEDUR)
-// =============================
-// Variabel koneksi
 $db_host = '127.0.0.1';
 $db_user = 'root';
 $db_pass = '';
-$db_name = 'kasir_db'; // <-- sesuai permintaan
+$db_name = 'kasir_db';
 
 $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-// Percabangan: jika gagal koneksi -> hentikan dan tampilkan pesan
 if (!$koneksi) {
     die('Koneksi gagal: ' . mysqli_connect_error());
 }
